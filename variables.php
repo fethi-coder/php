e103017e8d1cb943fba907316ca5749dd165f12f
fethi <fethibettahar9@gmail.com>
2021-03-14 15:21:06 +0100

    firstPHP

---
+++ variables.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+
+
+
+
+$title = 'indiana jones and the last crusade ';
+
+echo $title;
+
+
+
+$my_answer = true;
+
+echo $my_answer; 
+
+
+
+$release_year= '/1989/ ';
+
+echo $release_year;
+
+
+
+$note = 'sa note : 8.2 sur IMBD';
+
+echo $note;
+
+
+?>
+
+</body>
+</html>
+
+
+
+
+
+
+
